99f1f59f9534109ea364376388e40f82ca63e6a5
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-29 14:15:22 +0100

    solution complete

--- src/main/kotlin/easyProblems/twoSum_1/Solution.kt
+++ src/main/kotlin/easyProblems/twoSum_1/Solution.kt
@@ -1,7 +1,22 @@
 package easyProblems.twoSum_1
 
 class Solution {
-    fun twoSum(array: IntArray, target: Int): IntArray {
-        return intArrayOf(0, 1)
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        if (nums.size == 2) {
+            return intArrayOf(0, 1)
+        }
+
+        val mapOfSeenElements = mutableMapOf<Int, Int>()
+        for (index in nums.indices) {
+            val numberNeeded = target - nums[index]
+
+            if (mapOfSeenElements.containsKey(numberNeeded)) {
+                return intArrayOf(mapOfSeenElements[numberNeeded]!!, index)
+            }
+
+            mapOfSeenElements[nums[index]] = index
+        }
+
+        return intArrayOf()
     }
 }
--- src/test/kotlin/easyProblems/twoSum_1/SolutionTest.kt
+++ src/test/kotlin/easyProblems/twoSum_1/SolutionTest.kt
@@ -4,13 +4,37 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SolutionTest {
+    private val solution = Solution()
 
     @Test
-    fun `array has only two elements so the solution is always 0 and 1`() {
+    fun `array with two elements always returns 0, 1`() {
         val array = intArrayOf(1, 2)
         val target = 3
-        val solution = Solution()
 
         assertThat(solution.twoSum(array, target)).isEqualTo(intArrayOf(0, 1))
+    }
+
+    @Test
+    fun `array with first two elements add up to the target returns 0, 1`() {
+        val array = intArrayOf(1, 2, 3)
+        val target = 3
+
+        assertThat(solution.twoSum(array, target)).isEqualTo(intArrayOf(0, 1))
+    }
+
+    @Test
+    fun `should find correct indexes with array with elements to add up in random position`() {
+        val array = intArrayOf(3, 2, 4)
+        val target = 6
+
+        assertThat(solution.twoSum(array, target)).isEqualTo(intArrayOf(1, 2))
+    }
+
+    @Test
+    fun `should find correct indexes with longer array`() {
+        val array = intArrayOf(2, 9, 8, 4, 5, 3, 1, 1, 4, 5, 2, 8, 4, 6)
+        val target = 15
+
+        assertThat(solution.twoSum(array, target)).isEqualTo(intArrayOf(1, 13))
     }
 }
